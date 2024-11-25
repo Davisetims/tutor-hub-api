142d8b4a93d904785d5129bb8397b647637c3edf
DavisWere <devisodhis10@gmail.com>
2024-11-25 15:22:05 +0300

    "Added tutor SerializerMethodField to TutorAvailabilitySerializer and implemented get_tutor method to return tutor's first and last name."

--- tutor_availability/serializers.py
+++ tutor_availability/serializers.py
@@ -3,9 +3,19 @@ from tutor_availability.models import TutorAvailability
 
 
 class TutorAvailabilitySerializer(serializers.ModelSerializer):
+    tutor = serializers.SerializerMethodField()
     class Meta:
         model = TutorAvailability
         fields = ['tutor', 'day_of_week', 'hourly_rate']
+        
+    
+    def get_tutor(self, obj):
+        if obj.tutor:
+            return {
+                "first name": obj.tutor.first_name,
+                "last name": obj.tutor.last_name
+            }
+        return None
     
     def validate_tutor(self, tutor):
         if not tutor.is_tutor:
