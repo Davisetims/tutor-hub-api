e7ce5cc01f892c0b540450887b95152cb54e6cba
DavisWere <devisodhis10@gmail.com>
2024-11-25 15:35:47 +0300

    "Added SerializerMethodFields for tutor and course to UnitSerializer, with corresponding get methods."

--- courses/serializers.py
+++ courses/serializers.py
@@ -7,6 +7,24 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class UnitSerializer(serializers.ModelSerializer):
+    tutor =serializers.SerializerMethodField()
+    course = serializers.SerializerMethodField()
     class Meta:
         model = Unit
         fields  = '__all__'
+    
+    def get_tutor(self, obj):
+        if obj.tutor:
+            return {
+                "first name": obj.tutor.first_name,
+                "last name": obj.tutor.last_name
+            }
+        return None
+
+    def get_course(self, obj):
+        if obj.course:
+            return {
+                "title": obj.course.title,
+                "description": obj.course.description
+            }
+        return None
